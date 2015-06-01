035065f7d7264337f8009570414b16d094080aa6
Jmedina821 <medinajesus821@gmail.com>
2015-06-01 14:21:45 -0430

    Desplegables

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php 
-
 include("plantilla.php");
-
 ?>
-
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil porro, ipsa soluta enim maiores aspernatur, corporis dolores. Error laboriosam explicabo, porro quos sit delectus, impedit cum molestiae adipisci ipsa unde!
